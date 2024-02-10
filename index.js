f2e292cd68040136350dc049a3dc5625710c9e70
hardikhanda <hardikhanda.bt22cse@pec.edu.in>
2024-02-10 13:04:30 +0530

    setup

---
+++ index.js
@@ -0,0 +1,31 @@
+// server.js
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+
+const PORT = process.env.PORT || 3000;
+
+// Serve static files
+app.use(express.static('public'));
+
+// Socket.IO logic
+io.on('connection', (socket) => {
+    console.log('A user connected');
+
+    // Send notification to all connected clients
+    socket.on('sendNotification', (data) => {
+        io.emit('receiveNotification', data);
+    });
+
+    socket.on('disconnect', () => {
+        console.log('User disconnected');
+    });
+});
+
+server.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
